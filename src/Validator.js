82a43275cb2a98eded240af4849b4024a6ef13f4
Admin <Tgafvert@gmail.com>
2017-09-06 13:27:18 -0400

    verify

--- src/Validator.js
+++ src/Validator.js
@@ -2,15 +2,45 @@ import React, { Component } from 'react';
 import "./Validator.css";
 
 class Validator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {email: "", password1: '', password2: '', result: ''}
+     this.passwordChanged = this.passwordChanged.bind(this)
+     this.password2Changed = this.password2Changed.bind(this)
+  }
+
+
+
+  passwordChanged (e) {
+    console.log(e.target.value)
+    this.setState({password1: e.target.value})
+
+  }
+  password2Changed (e) {
+    console.log(e.target.value)
+    this.setState({password2: e.target.value})
+
+  }
+
+  checkPassOnSubmit (e) {
+    console.log(e.target.value)
+
+
+  }
+
+
+
   render() {
+    let result = "Valid!"
     return (
-      <div className="form">
+      <form>
         <h1>Sign Up</h1>
         <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <input type="password" placeholder="confirm password" />
-        <input type="submit" value="Submit" />
-      </div>
+        <input type="password" placeholder="password" onChange={this.passwordChanged}/>
+        <input type="password" placeholder="confirm password" onChange={this.password2Changed} />
+        <input type="submit" value="Submit" onClick={this.checkPassOnSubmit}/>
+        <p>{result}</p>
+      </form>
     );
   }
 }
